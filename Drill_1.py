9f72981895680b5ffb42cff5beb7591ae40060ce
jaejoonlee <lhw5922@gmail.com>
2024-09-16 16:11:34 +0900

    Drill 1 제출 (2022180032 이재준)

---
+++ Drill_1.py
@@ -0,0 +1,160 @@
+Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py
+
+= RESTART: C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py =
+
+= RESTART: C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py =
+
+= RESTART: C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py =
+
+= RESTART: C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py =
+
+= RESTART: C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py =
+Exception in Tkinter callback
+Traceback (most recent call last):
+  File "C:\Python\Lib\tkinter\__init__.py", line 1968, in __call__
+    return self.func(*args)
+           ^^^^^^^^^^^^^^^^
+  File "C:\Python\Lib\turtle.py", line 676, in eventfun
+    fun()
+  File "C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py", line 41, in move_left
+    t.setheading(180)
+  File "C:\Python\Lib\turtle.py", line 1935, in setheading
+    self._rotate(angle)
+  File "C:\Python\Lib\turtle.py", line 3348, in _rotate
+    self._update()
+  File "C:\Python\Lib\turtle.py", line 2668, in _update
+    self._update_data()
+  File "C:\Python\Lib\turtle.py", line 2654, in _update_data
+    self.screen._incrementudc()
+  File "C:\Python\Lib\turtle.py", line 1284, in _incrementudc
+    raise Terminator
+turtle.Terminator
+Exception in Tkinter callback
+Traceback (most recent call last):
+  File "C:\Python\Lib\tkinter\__init__.py", line 1968, in __call__
+    return self.func(*args)
+           ^^^^^^^^^^^^^^^^
+  File "C:\Python\Lib\turtle.py", line 676, in eventfun
+    fun()
+  File "C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py", line 47, in move_right
+    t.setheading(0)
+  File "C:\Python\Lib\turtle.py", line 1935, in setheading
+    self._rotate(angle)
+  File "C:\Python\Lib\turtle.py", line 3348, in _rotate
+    self._update()
+  File "C:\Python\Lib\turtle.py", line 2668, in _update
+    self._update_data()
+  File "C:\Python\Lib\turtle.py", line 2658, in _update_data
+    self.screen._drawline(self.currentLineItem, self.currentLine,
+  File "C:\Python\Lib\turtle.py", line 539, in _drawline
+    self.cv.coords(lineitem, *cl)
+  File "<string>", line 1, in coords
+  File "C:\Python\Lib\tkinter\__init__.py", line 2851, in coords
+    self.tk.call((self._w, 'coords') + args))]
+    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+_tkinter.TclError: invalid command name ".!canvas"
+Exception in Tkinter callback
+Traceback (most recent call last):
+  File "C:\Python\Lib\tkinter\__init__.py", line 1968, in __call__
+    return self.func(*args)
+           ^^^^^^^^^^^^^^^^
+  File "C:\Python\Lib\turtle.py", line 676, in eventfun
+    fun()
+  File "C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py", line 29, in move_up
+    t.setheading(90)
+  File "C:\Python\Lib\turtle.py", line 1935, in setheading
+    self._rotate(angle)
+  File "C:\Python\Lib\turtle.py", line 3348, in _rotate
+    self._update()
+  File "C:\Python\Lib\turtle.py", line 2668, in _update
+    self._update_data()
+  File "C:\Python\Lib\turtle.py", line 2658, in _update_data
+    self.screen._drawline(self.currentLineItem, self.currentLine,
+  File "C:\Python\Lib\turtle.py", line 539, in _drawline
+    self.cv.coords(lineitem, *cl)
+  File "<string>", line 1, in coords
+  File "C:\Python\Lib\tkinter\__init__.py", line 2851, in coords
+    self.tk.call((self._w, 'coords') + args))]
+    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+_tkinter.TclError: invalid command name ".!canvas"
+Exception in Tkinter callback
+Traceback (most recent call last):
+  File "C:\Python\Lib\tkinter\__init__.py", line 1968, in __call__
+    return self.func(*args)
+           ^^^^^^^^^^^^^^^^
+  File "C:\Python\Lib\turtle.py", line 676, in eventfun
+    fun()
+  File "C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py", line 47, in move_right
+    t.setheading(0)
+  File "C:\Python\Lib\turtle.py", line 1935, in setheading
+    self._rotate(angle)
+  File "C:\Python\Lib\turtle.py", line 3350, in _rotate
+    self._update()
+  File "C:\Python\Lib\turtle.py", line 2668, in _update
+    self._update_data()
+  File "C:\Python\Lib\turtle.py", line 2658, in _update_data
+    self.screen._drawline(self.currentLineItem, self.currentLine,
+  File "C:\Python\Lib\turtle.py", line 539, in _drawline
+    self.cv.coords(lineitem, *cl)
+  File "<string>", line 1, in coords
+  File "C:\Python\Lib\tkinter\__init__.py", line 2851, in coords
+    self.tk.call((self._w, 'coords') + args))]
+    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+_tkinter.TclError: invalid command name ".!canvas"
+Exception in Tkinter callback
+Traceback (most recent call last):
+  File "C:\Python\Lib\tkinter\__init__.py", line 1968, in __call__
+    return self.func(*args)
+           ^^^^^^^^^^^^^^^^
+  File "C:\Python\Lib\turtle.py", line 676, in eventfun
+    fun()
+  File "C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py", line 35, in move_down
+    t.setheading(270)
+  File "C:\Python\Lib\turtle.py", line 1935, in setheading
+    self._rotate(angle)
+  File "C:\Python\Lib\turtle.py", line 3348, in _rotate
+    self._update()
+  File "C:\Python\Lib\turtle.py", line 2668, in _update
+    self._update_data()
+  File "C:\Python\Lib\turtle.py", line 2658, in _update_data
+    self.screen._drawline(self.currentLineItem, self.currentLine,
+  File "C:\Python\Lib\turtle.py", line 539, in _drawline
+    self.cv.coords(lineitem, *cl)
+  File "<string>", line 1, in coords
+  File "C:\Python\Lib\tkinter\__init__.py", line 2851, in coords
+    self.tk.call((self._w, 'coords') + args))]
+    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+_tkinter.TclError: invalid command name ".!canvas"
+
+= RESTART: C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py =
+
+= RESTART: C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py =
+Traceback (most recent call last):
+  File "C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py", line 4, in <module>
+    window.title("WASD로 거북이 이동")
+NameError: name 'window' is not defined
+>>> 
+= RESTART: C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py =
+>>> 
+= RESTART: C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py =
+>>> 
+= RESTART: C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py =
+>>> 
+= RESTART: C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py =
+>>> 
+= RESTART: C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py =
+>>> 
+= RESTART: C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py =
+Traceback (most recent call last):
+  File "C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py", line 4, in <module>
+    window.listen()
+NameError: name 'window' is not defined
+>>> 
+= RESTART: C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py =
+>>> 
+= RESTART: C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py =
+>>> 
+= RESTART: C:/Users/lhw59/OneDrive/바탕 화면/대학교/대학교/2학년 2학기/2d 게임프로그래밍/drill_1.py =
