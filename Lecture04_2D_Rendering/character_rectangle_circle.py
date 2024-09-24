a60ec9498fd25907f37ee566bee7891a8a4f9c08
jaejoonlee <lhw5922@gmail.com>
2024-09-24 09:51:33 +0900

    전체적인 구조 만들기
    사각형-원 무한 이동의 전체적인 구조

---
+++ Lecture04_2D_Rendering/character_rectangle_circle.py
@@ -0,0 +1,21 @@
+from pico2d import *
+
+def run_rectangle():
+    pass
+
+def run_circle():
+    pass
+
+
+open_canvas()
+
+#fill here
+grass = load_image('grass.png')
+boy = load_image('character.png')
+
+while true:
+    run_circel()
+    run_rectangle()
+
+
+close_canvas()
