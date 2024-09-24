1deb81abbf9597d35a82d82da52791fd265a5e94
jaejoonlee <lhw5922@gmail.com>
2024-09-24 10:44:03 +0900

    원 이동 및 오른쪽 위 이동 완료

--- Lecture04_2D_Rendering/character_rectangle_circle.py
+++ Lecture04_2D_Rendering/character_rectangle_circle.py
@@ -7,8 +7,15 @@ open_canvas()
 grass = load_image('grass.png')
 boy = load_image('character.png')
 
+def draw_boy(x,y):
+    clear_canvas_now()
+    boy.draw_now(x,y)
+    delay(0.1)
+
 def run_top():
     print('TOP')
+    for x in range(0,800,10):
+        draw_boy(x,550)
     pass
 def run_right():
     print('RIGHT')
@@ -38,15 +45,12 @@ def run_circle():
         x = cx + r * math.cos(math.radians(d))
         y = cy + r * math.sin(math.radians(d))
 
-        clear_canvas_now()
-        boy.draw_now(x,y)
-        delay(0.1)
-
+        draw_boy(x,y)
 pass
 
 
 while True:
-    #run_circle()
+    run_circle()
     run_rectangle()
     break
 
