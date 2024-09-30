4c53a5f9fa967a7e509e700e54a85393d2fd90c7
jaejoonlee <lhw5922@gmail.com>
2024-09-30 21:45:45 +0900

    dir_y 추가, dir->dir_x로 변경, 상하 이동 추가

--- Labs/Lecture06_HandlingInputs/move_my_character_with_key.py
+++ Labs/Lecture06_HandlingInputs/move_my_character_with_key.py
@@ -6,7 +6,7 @@ character = load_image('character_walk_sprites.png')
 
 
 def handle_events():
-    global running, dir
+    global running, dir_x, dir_y
 
     events = get_events()
     for event in events:
@@ -14,30 +14,41 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                dir += 1
+                dir_x += 1
             elif event.key == SDLK_LEFT:
-                dir -= 1
+                dir_x -= 1
+            elif event.key == SDLK_UP:
+                dir_y += 1
+            elif event.key == SDLK_DOWN:
+                dir_y -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
-                dir -= 1
+                dir_x -= 1
             elif event.key == SDLK_LEFT:
-                dir += 1
+                dir_x += 1
+            elif event.key == SDLK_UP:
+                dir_y -= 1
+            elif event.key == SDLK_DOWN:
+                dir_y += 1
 
 running = True
 x = 800 // 2
+y = 90
 frame = 0
-dir = 0
+dir_x = 0
+dir_y = 0
 
 while running:
     clear_canvas()
     grass.draw(400, 100)
-    character.clip_draw(frame*240 + 80, 10, 190, 200, x, 90)
+    character.clip_draw(frame * 240 + 80, 10, 190, 200, x, y)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 10
-    x += dir * 5
+    x += dir_x * 5
+    y += dir_y * 5
     delay(0.05)
 
 
