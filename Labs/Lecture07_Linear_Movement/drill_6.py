240bed96ca7773025c7f9f5ac33931fed318efd5
jaejoonlee <lhw5922@gmail.com>
2024-10-04 16:21:35 +0900

    기본 틀

---
+++ Labs/Lecture07_Linear_Movement/drill_6.py
@@ -0,0 +1,20 @@
+from pico2d import *
+
+WIDTH, HEIGHT = 1280, 1024
+open_canvas(WIDTH, HEIGHT)
+
+ground = load_image('TUK_GROUND.png')
+character = load_image('animation_sheet.png')
+
+def move_hand():
+
+    pass
+
+
+
+def move_boy():
+
+    pass
+
+
+close_canvas()
