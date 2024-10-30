d3c4b23f3c781a610f843501cae39da68349c913
jaejoonlee <lhw5922@gmail.com>
2024-10-30 20:16:35 +0900

    AutoRun에서 상태변화

--- Labs/Lecture10_Character_Controller_1/boy.py
+++ Labs/Lecture10_Character_Controller_1/boy.py
@@ -134,7 +134,7 @@ class Boy:
                 Run : {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle, space_down: Run}, # Run 상태에서 어떤 이벤트가 들어와도 처리하지 않겠다
                 Idle : { right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out: Sleep, space_down: Idle },
                 Sleep : { right_down: Run, left_down: Run, right_up: Run, left_up: Run, space_down: Idle },
-                AutoRun : {}
+                AutoRun : {time_out: Idle, right_down: Run, left_down: Run, right_up: Run, left_up: Run}
             }
         )
 
