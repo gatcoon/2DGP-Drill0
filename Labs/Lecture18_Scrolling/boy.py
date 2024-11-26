75fa3ad3b813f9515415255daa7eead520d029c2
jaejoonlee <lhw5922@gmail.com>
2024-11-26 11:11:13 +0900

    11.26 강의내용

--- Labs/Lecture18_Scrolling/boy.py
+++ Labs/Lecture18_Scrolling/boy.py
@@ -204,7 +204,11 @@ class Boy:
         )
 
         # modify here
-        self.x, self.y = get_canvas_width() / 2, get_canvas_height() / 2
+        # self.x, self.y = get_canvas_width() / 2, get_canvas_height() / 2
+
+        # 전체 필드 배경의 중심
+        self.x, self.y = server.background.w / 2, server.background.h / 2
+
 
 
 
@@ -215,15 +219,26 @@ class Boy:
         self.x += math.cos(self.dir) * self.speed * game_framework.frame_time
         self.y += math.sin(self.dir) * self.speed * game_framework.frame_time
 
-        self.x = clamp(25.0, self.x, get_canvas_width()-25.0)
-        self.y = clamp(25.0, self.y, get_canvas_height()-25.0)
+        # 화면 좌표 단위의 상하좌우제한
+        # self.x = clamp(25.0, self.x, get_canvas_width()-25.0)
+        # self.y = clamp(25.0, self.y, get_canvas_height()-25.0)
+
+        # 월드 좌표 관점의 상하좌우제한하려면
+        # self.x = clamp(25.0, self.x, server.background.w - 25.0)
+        # self.y = clamp(25.0, self.y, server.background.h - 25.0)
+
 
     def handle_event(self, event):
         self.state_machine.handle_event(('INPUT', event))
 
     def draw(self):
-        self.image.clip_draw(int(self.frame) * 100, self.action * 100, 100, 100, self.x, self.y)
-        self.font.draw(int(self.x - 100), int(self.y + 60), f'({self.x:5.5}, {self.y:5.5})', (255, 255, 0))
+        sx, sy = get_canvas_width()/2, get_canvas_height()/2
+
+        # sx = self.x - server.background.window_left
+        # sy = self.y - server.background.window_bottom
+
+        self.image.clip_draw(int(self.frame) * 100, self.action * 100, 100, 100, sx, sy)
+        self.font.draw(int(sx - 100), int(sy + 60), f'({self.x:5.5}, {self.y:5.5})', (255, 255, 0))
 
 
     def get_bb(self):
